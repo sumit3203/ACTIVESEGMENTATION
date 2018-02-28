542187f69d9baa3d4e4389fea9dd83032d173d00
dprodanov <dimiterpp@gmail.com>
2018-02-28 23:37:31 +0100

    testing

--- src/Active_Segmentation_.java
+++ src/Active_Segmentation_.java
@@ -1,37 +1,23 @@
 
 import java.awt.Panel;
-
+import java.io.File;
 
 import javax.swing.SwingUtilities;
 
 import activeSegmentation.IProjectManager;
-import activeSegmentation.IFeatureManager;
-import activeSegmentation.IFilterManager;
-import activeSegmentation.ILearningManager;
-import activeSegmentation.feature.FeatureManager;
-import activeSegmentation.feature.PixelInstanceCreator;
-import activeSegmentation.feature.RoiInstanceCreator;
-import activeSegmentation.filterImpl.FilterManager;
 import activeSegmentation.gui.CreatProject;
-import activeSegmentation.gui.GuiController;
 import activeSegmentation.io.ProjectManagerImp;
-import activeSegmentation.learning.ClassifierManager;
 import ij.IJ;
-import ij.ImageJ;
 import ij.plugin.PlugIn;
 
 public class Active_Segmentation_ implements PlugIn {
 
-
-
-
 	/** main GUI panel (containing the buttons panel on the left,
 	 *  the image in the center and the annotations panel on the right */
 	Panel all = new Panel();
-	public Active_Segmentation_(){
-
-	}
-
+	
+	private final String filesep = System.getProperty("file.separator");
+	private String jarpath=filesep+"jars"+filesep;
 	
 	/**
 	 * This method will be an entry point into the Plugin. All the
@@ -42,17 +28,14 @@ public class Active_Segmentation_ implements PlugIn {
 	 */
 	@Override
 	public void run(String arg0) {
-		 new ImageJ();
+		//new ImageJ();
 		IJ.log(System.getProperty("plugins.dir"));
-		String home = System.getProperty("plugins.dir")+"\\plugins\\activeSegmentation\\";
+		String home = System.getProperty("plugins.dir")+jarpath;//+"\\plugins\\activeSegmentation\\";
+		System.out.println("jars home:  "+home);
+		//String home = System.getProperty("plugins.dir")+"\\plugins\\activeSegmentation\\";
 		
        // System.out.println(Active_Segmentation_.class.getProtectionDomain().getCodeSource().getLocation());
 		IProjectManager dataManager= new ProjectManagerImp();
-		
-	
-		
-		
-		//ILearningManager  learningManager= new ClassifierManager(dataManager);
 		
 		//System.out.println(home);
        CreatProject creatProject= new CreatProject(dataManager);
@@ -64,9 +47,23 @@ public class Active_Segmentation_ implements PlugIn {
 	
 
 	public static void main(String[] args) {
-		// new ij.ImageJ();
+		System.out.println(args[0]);
+		try {
+			File f=new File(args[0]);
+			if (f.exists() && f.isDirectory() ) {
 
-		new Active_Segmentation_().run("");
+				System.setProperty("plugins.dir", args[0]);
+				new Active_Segmentation_().run("");
+
+			} else {
+				throw new IllegalArgumentException();
+			}
+		} catch (Exception ex) {
+			IJ.log("plugins.dir misspecified\n");
+			ex.printStackTrace();
+		}
+
+
 	}
 
 }
