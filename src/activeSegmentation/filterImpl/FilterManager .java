1e3888d0a9a43ccbe17f1e27df0e472fd756caa2
getsanjeev <getsanjeevdubey@gmail.com>
2018-07-29 21:12:10 +0530

    updated filtermanger to avoid conflict

--- src/activeSegmentation/filterImpl/FilterManager .java
+++ src/activeSegmentation/filterImpl/FilterManager .java
@@ -29,23 +29,16 @@ import java.util.zip.ZipInputStream;
 import activeSegmentation.Common;
 import activeSegmentation.FeatureType;
 import activeSegmentation.IFeatureManagerNew;
-import activeSegmentation.filterImpl.ApplyZernikeFilter;
-import activeSegmentation.gui.FeaturePanelNew;
-import activeSegmentation.gui.Gui;
 import activeSegmentation.IProjectManager;
 import activeSegmentation.LearningType;
 import activeSegmentation.IFilter;
 import activeSegmentation.IFilterManager;
 import activeSegmentation.ProjectType;
-import activeSegmentation.feature.FeatureManagerNew;
 import activeSegmentation.io.ProjectInfo;
-import activeSegmentation.io.ProjectManagerImp;
 import ij.IJ;
-import ij.ImageJ;
 import ij.ImagePlus;
 import ij.ImageStack;
 import ij.gui.Roi;
-import ij.process.ImageConverter;
 import ijaux.scale.Pair;
 import ijaux.scale.ZernikeMoment.Complex;
 
@@ -195,12 +188,8 @@ public class FilterManager extends URLClassLoader implements IFilterManager {
 					for(String image: images) {
 						for(String key: featureManager.getClassKeys()) {
 							List<Roi> rois=featureManager.getExamples(key, LearningType.BOTH.name(), image);
-							// convert to gray-scale
-							ImagePlus imp = new ImagePlus(projectString+image);
-							ImageConverter ic= new ImageConverter(imp);
-					        ic.convertToGray8();
 							if(rois!=null && !rois.isEmpty()) {
-								filter.applyFilter(imp.getProcessor(),
+								filter.applyFilter(new ImagePlus(projectString+image).getProcessor(),
 										filterString+image.substring(0, image.lastIndexOf(".")),
 										rois);
 								if(filter.getFeatures()!=null) {
@@ -371,7 +360,6 @@ public class FilterManager extends URLClassLoader implements IFilterManager {
 
 		return filterMap.get(key).getImage();
 	}
-	
 
 
 }
