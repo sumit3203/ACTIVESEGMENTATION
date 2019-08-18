1ef80504c7f748aa063956965f52d641bdd3554d
sumit <sumit.3203@gmail.com>
2019-08-18 14:39:06 +0200

    Order of images #47

--- src/activeSegmentation/io/ProjectManagerImp.java
+++ src/activeSegmentation/io/ProjectManagerImp.java
@@ -16,10 +16,14 @@ import java.io.OutputStreamWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -137,7 +141,7 @@ public class ProjectManagerImp implements IProjectManager {
 				//System.out.println( projectInfo.getProjectName());
 				setProjectDir(projectFile.getParent(), null);
 				projectInfo.setProjectDirectory(projectDir);
-
+			
 			} catch (JsonGenerationException e) {
 				e.printStackTrace();
 			} catch (JsonMappingException e) {
@@ -305,15 +309,49 @@ public class ProjectManagerImp implements IProjectManager {
 		IJ.log("DONE");
 	}
 
+	
+	private File[] sortImages(File[] images) {
+		final Pattern p = Pattern.compile("\\d+");
+		Arrays.sort(images, new  Comparator<File>(){
+			@Override public int compare(File o1, File o2) {
+				Matcher m = p.matcher(o1.getName());
+				Integer number1 = null;
+				if (!m.find()) {
+					return o1.getName().compareTo(o2.getName());
+				}
+				else {
+					Integer number2 = null;
+					number1 = Integer.parseInt(m.group());
+					m = p.matcher(o2.getName());
+					if (!m.find()) {
+						return o1.getName().compareTo(o2.getName());
+					}
+					else {
+						number2 = Integer.parseInt(m.group());
+						int comparison = number1.compareTo(number2);
+						if (comparison != 0) {
+							return comparison;
+						}
+						else {
+							return o1.getName().compareTo(o2.getName());
+						}
+					}
+				}
+			}}
+				);
+		return images;
+	}
 	private List<String> loadImages(String directory){
 		List<String> imageList= new ArrayList<String>();
 		File folder = new File(directory);
-		File[] images = folder.listFiles();
+		File[] images = sortImages(folder.listFiles());
+		
 		for (File file : images) {
 			if (file.isFile()) {
 				imageList.add(file.getName());
 			}
 		}
+		
 		return imageList;
 	}
 	private boolean createDirectory(String project){
