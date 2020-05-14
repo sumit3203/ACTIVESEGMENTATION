e64dd27278e8904258c7ebbad8d7e8dd0d3f38ac
Raghavendrapara <41910672+Raghavendrapara@users.noreply.github.com>
2020-05-14 21:13:59 +0530

    Update IFilter.java

--- src/activeSegmentation/IFilter.java
+++ src/activeSegmentation/IFilter.java
@@ -3,10 +3,7 @@ package activeSegmentation;
 
 import ij.gui.Roi;
 import ij.process.ImageProcessor;
-//import ijaux.datatype.Pair;
-//Can be replaced by existing implementation in javafx
-import javafx.util.Pair;
-
+import ijaux.datatype.Pair;
 import java.lang.reflect.Field;
 import java.util.Enumeration;
 import java.util.HashMap;
