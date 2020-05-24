6a7dad0608ca79b774797950387f22127491d60c
prodanov <prodanov@ICT0120821.imec.be>
2020-05-24 13:13:43 +0200

    Filter annotations

--- src/activeSegmentation/AFilter.java
+++ src/activeSegmentation/AFilter.java
@@ -3,9 +3,28 @@ package activeSegmentation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Annotation mechanism for tagging classes
+ * @author prodanov
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AFilter {
+	/**
+	 * unique key used in the GUI 
+	 * @return
+	 */
    public String key();
+   /**
+    * The value is displayed in the GUI as a tab title
+    * @return
+    */
    public String value();
+   
+   /**
+    * The type is used in the loading mechanism
+    * 
+    * @return
+    */
    public FilterType type();
 }
--- src/activeSegmentation/AFilterField.java
+++ src/activeSegmentation/AFilterField.java
@@ -5,7 +5,21 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 
+/**
+ *  Annotation mechanism for tagging fields of classes
+ * @author prodanov
+ *
+ */
 public @interface AFilterField {
+	/**
+	 * unique key
+	 * @return
+	 */
 	public String key() default "";
+	
+	/**
+	 * The value is displayed in the GUI
+	 * @return
+	 */
 	public String value() default "";
 }
